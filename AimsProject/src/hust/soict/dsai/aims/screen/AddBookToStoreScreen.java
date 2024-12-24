78beff7683f8c6552201e883c2b7af394e586c7d
vanh <leanh2601@gmail.com>
2024-12-24 21:52:20 +0700

    Hoan thien Update Store

--- AimsProject/src/hust/soict/dsai/aims/screen/AddBookToStoreScreen.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/AddBookToStoreScreen.java
@@ -3,10 +3,11 @@ package hust.soict.dsai.aims.screen;
 import javax.swing.JFrame;
 
 import hust.soict.dsai.aims.store.Store;
+import hust.soict.dsai.aims.cart.Cart;
 
 public class AddBookToStoreScreen extends AddItemToStoreScreen {
-    public AddBookToStoreScreen(JFrame storeScreen, JFrame cartScreen, Store store) {
-        super(storeScreen, cartScreen, store);
+    public AddBookToStoreScreen(JFrame storeScreen, JFrame cartScreen, Store store, Cart cart) {
+        super(storeScreen, cartScreen, store, cart);
     }
 
     @Override
@@ -19,4 +20,3 @@ public class AddBookToStoreScreen extends AddItemToStoreScreen {
     	return AddBookToStoreScreen.this;
     }
 }
-
